f295ff28aace6745df65d9a337966c19aa8b7f43
[Adnan] <am6377@nyu.edu>
2017-03-13 00:53:44 -0400

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,45 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+var socket = io();
+
+console.log(name + ' wants to join ' + room);
+
+// Update h1 tag
+jQuery('.room-title').text(room);
+
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
+});
+
+socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
+	var $messages = jQuery('.messages');
+	var $message = jQuery('<li class="list-group-item"></li>');
+
+	console.log('New message:');
+	console.log(message.text);
+
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
+	$messages.append($message);
+});
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		name: name,
+		text: $message.val()
+	});
+
+	$message.val('');
+});
---
+++ server.js
@@ -0,0 +1,85 @@
+var PORT = process.env.PORT || 3000;
+var moment = require('moment');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+var clientInfo = {};
+
+// Sends current users to provided socket
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+}
+
+io.on('connection', function (socket) {
+	console.log('User connected via socket.io!');
+
+	socket.on('disconnect', function () {
+		var userData = clientInfo[socket.id];
+
+		if (typeof userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left!',
+				timestamp: moment().valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
+	socket.on('joinRoom', function (req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined!',
+			timestamp: moment().valueOf()
+		});
+	});
+
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
+
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);	
+		}
+	});
+
+	// timestamp property - JavaScript timestamp (milliseconds)
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
+	});
+});
+
+http.listen(PORT, function () {
+	console.log('Server started!');
+});
